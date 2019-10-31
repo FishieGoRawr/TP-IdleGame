b3eded4ccfc4548109ea4011f1afab229aafb59c
Hinnel <rachel.hince@hotmail.fr>
2019-10-31 11:13:50 -0400

    Methode de base SqlExecutor

--- ProgressQuest Client/SqlExecutor.cs
+++ ProgressQuest Client/SqlExecutor.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ProgressQuest_Client
 {
     class SqlExecutor
     {
+        public void changeDB(string change)
+        {
+
+        }
+
+        public void changeServer(string change)
+        {
+
+        }
+
+        public bool testConnexion()
+        {
+            return true;
+        }
+
+        //public DataTable extractTable()
+        //{
+        //    return 
+        //}
+
+        //public DataTable extractFunction()
+        //{
+        //    return;
+        //}
+
+        //Revoir le retour de classe
+        public void extractFunction(string nomFunction, SqlParameter[] param)
+        {
+
+        }
+
+        public void executeProc_NoParam()
+        {
+
+        }
+
+        public void executeProc_WithParam()
+        {
+
+        }
+
+        //Revoir le retour de classe
+        public void executeProc_Id()
+        {
+
+        }
     }
 }
